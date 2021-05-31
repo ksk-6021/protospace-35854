caad9cace235c721a0f63e37b0eb98041f834ee8
ksk-6021 <sgkg2215462@gmail.com>
2021-05-31 16:31:40 +0900

    他のユーザーに編集画面を表示させない処理修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -26,7 +26,7 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in?
+    unless @prototype.id == @prototype.user_id
       redirect_to action: :index
     end
   end
